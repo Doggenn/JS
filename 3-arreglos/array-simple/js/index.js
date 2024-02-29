7b3cd01bb25b37de165b26fe5fbe6f3b48a313bd
Doggenn <19arg09@gmail.com>
2024-02-29 10:45:04 +0100

    Ejercicios de JS

---
+++ 3-arreglos/array-simple/js/index.js
@@ -0,0 +1,10 @@
+
+// Ambos hacen los mismo: \/
+const letras = [];// <--- \/
+const numeros = new Array();
+
+
+
+// Array con valores.
+const empleados = ['Maria', 'Julia', 'Ines', 'Victor'];
+const pares = new Array(2, 4, 6, 8, 10);

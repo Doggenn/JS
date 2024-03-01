eb1a843debf1ccbaf234214f6970d581abb96be7
Doggenn <19arg09@gmail.com>
2024-03-01 17:24:50 +0100

    Ejercicios de JS

---
+++ 3-arreglos/funciones-de-arrays/funciones-de-busqueda(find)/js/index.js
@@ -0,0 +1,29 @@
+/*
+FIND Busca dentrp de un array un elemento que cumpla cierta condicion.
+Find devuelve un unico elemento. El primero que encuentre que cumpla la condicion
+*/
+
+const frutas = ['manzana', 'pera', 'platano', 'uvas'];
+
+const frutaBuscada = frutas.find((fruta) => fruta === 'pera');
+
+const nombres = ['Ana', 'Pepe', 'Maria', 'Lucia'];
+
+let maximaLongitud = 0;
+const nombreLargo = nombres.find((nombre) => {
+    if (nombre.length < maximaLongitud) {
+        maximaLongitud = nombre.length
+    }
+    maximaLongitud++
+})
+
+console.log(frutaBuscada)
+
+
+
+const alumnos = [
+    { nombre: 'Juan', nota: 3 },
+    { nombre: 'Mario', nota: 4 },
+    { nombre: 'Ana', nota: 6 },
+    { nombre: 'Darwin', nota: 8 },
+]

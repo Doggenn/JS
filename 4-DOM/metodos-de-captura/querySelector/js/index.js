b073f92f486b44ba11ef88bce19573015f76cb5a
Doggenn <19arg09@gmail.com>
2024-03-04 17:41:22 +0100

    Ejercicios de JS

--- 4-DOM/metodos-de-captura/querySelector/js/index.js
+++ 4-DOM/metodos-de-captura/querySelector/js/index.js
@@ -1,1 +1,15 @@
 // Hay dos metodos querySelector y querySelectorAll
+
+//Capturar la clase menu del footer
+
+const menuFooter = document.querySelector('footer .menu');
+
+const arraySecciones = ['home', 'contacto', 'servicios', 'galeria', 'cookies']
+
+function pintarArray(lista) {
+    lista.forEach((seccion) => {
+        menuFooter.innerHTML += `<li><a href="${seccion}.html">${seccion}</a></li>`
+    })
+}
+
+pintarArray(arraySecciones);
